8b4c0d014b19f0ac695a49703592a07b7a1a49f2
R.C. Long <deac.long@wsu.edu>
2018-03-27 03:06:15 -0700

    Create ImageSearch class and hook up to imageSearch router

---
+++ src/controllers/imageSearch.controller.js
@@ -0,0 +1,10 @@
+class ImageSearch {
+  static search(req, res) {
+
+  }
+  static getSearchHistory(req, res) {
+
+  }
+}
+
+module.exports = ImageSearch
--- src/routes/imageSearch.routes.js
+++ src/routes/imageSearch.routes.js
@@ -1,13 +1,15 @@
+const ImageSearch = require('../controllers/imageSearch.controller.js')
+
 var routes = function (app) {
   app.get("/", function (req, res) {
     return res.send("Server is working")
   })
   app.get("/imagesearch/:input", function (req, res) {
-
+    ImageSearch.search(req, res)
   });
 
   app.get("/searchhistory", function (req, res) {
-
+    ImageSearch.getSearchHistory(req, res)
   });
 }
 
